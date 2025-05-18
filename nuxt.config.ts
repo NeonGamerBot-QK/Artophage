cd7ff6555f21233bb661d749a073df9997994d71
Saahil <neon@saahild.com>
2025-05-18 19:58:45 -0400

    update: tw css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  modules: ['@nuxtjs/supabase', '@nuxtjs/turnstile', '@nuxtjs/turnstile'],
+  modules: ['@nuxtjs/supabase'],
   devtools: { enabled: true },
   hub: {
     database: true
   },
-
+  supabase: {
+    redirect: false,
+  },
+vite: {
+  plugins: [tailwindcss()],
+},
+  css: ["~/assets/css/app.css"],
 })
