adef03b9365349909c10656388071bdf105ded69
Saahil <neon@saahild.com>
2025-05-21 19:16:11 -0400

    update: ye-stodays chanegs

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 </script>
 <template>
   <div>
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer sm:footer-horizontal bg-base-200 text-neutral-content items-center p-4 bottom-0 absolute">
+  <aside class="grid-flow-col items-center">
+    <p>Copyright © {{new Date().getFullYear()}} - All right reserved</p>
+  </aside>
+</footer>
+</template>
