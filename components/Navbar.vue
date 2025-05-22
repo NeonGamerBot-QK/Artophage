4391d19636772206608520ef5fe3655ad533d7c1
Saahil <neon@saahild.com>
2025-05-21 20:12:11 -0400

    update: add login/logout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,23 +1,44 @@
+<script setup lang="ts">
+import  md5  from "md5"
+let isLoggedIn = false;
+
+let avatarURL = null;
+if(isLoggedIn) {
+let userData = { email: "neon@saahild.com"}
+
+  avatarURL = `https://gravatar.com/avatar/${md5(userData.email)}?d=identicon`;
+}
+</script>
 <template>
     <div class="navbar bg-base-100 shadow-sm">
   <div class="flex-1">
     <a class="btn btn-ghost text-xl">Artophage</a>
   </div>
   <div class="flex-none">
-   
     <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+      <div v-if="isLoggedIn">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
           <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            alt="Your avatar"
+            :src="avatarURL" />
         </div>
       </div>
       <ul
         tabindex="0"
         class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Logout</a></li>
+        <li><a href="/api/logout">Logout</a></li>
       </ul>
+      </div>
+      <div v-else>
+ <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a href="/api/login">Login</a></li>
+      </ul>
+    </div>
+      </div>
     </div>
   </div>
 </div>
