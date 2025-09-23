8293e22e6b13dde75aa018222ce5497b930b8420
Neon <neon@saahild.com>
2025-09-23 23:51:17 +0000

    e

---
+++ server/api/login-email.ts
@@ -0,0 +1,10 @@
+export default eventHandler(async (event) => {
+    const body = await readBody(event)
+    const { email } = body;
+    if(!email)    throw createError({
+        statusCode: 400,
+        statusMessage: "Missing email"
+      })
+    // send email via node mailer  
+    
+})
