5fcf4d8905f3724b8a1ab525648ce659c0f7980c
Neon <neon@saahild.com>
2025-09-25 15:16:14 +0000

    e

--- server/api/auth/callback.ts
+++ server/api/auth/callback.ts
@@ -1,4 +1,19 @@
-import { jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
 export default defineEventHandler((event) => {
-  // jwt here
+  const query = getQuery(event);
+  const token = query.token;
+
+  if (!token) {
+    throw createError({ statusCode: 401, statusMessage: "Missing token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // or public key if RS256
+   console.log(decoded)
+    // valid token
+    return { message: "JWT valid", user: decoded };
+  } catch (err) {
+    throw createError({ statusCode: 403, statusMessage: "Invalid or expired token" });
+  }
 });
